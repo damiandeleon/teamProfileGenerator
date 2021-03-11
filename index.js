6ae1c1195f5548be62edee1168cc71bd206e6a2e
damian.deleon <damianmdeleon@gmail.com>
2021-03-11 10:31:16 -0600

    updated javascript

--- index.js
+++ index.js
@@ -3,68 +3,133 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'managerName',
-            message: "What is the Manager's name?",
-        },
-        {
-            type: 'input',
-            name: 'managerID',
-            message: "What is the Manager's Employee ID?",
-        },
-        {
-            type: 'input',
-            name: 'managerEmail',
-            message: "What is the Manager's email address?",
-        },
-        {
-            type: 'input',
-            name: 'managerPhone',
-            message: "What is the Manager's phone number?",
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'How would someone try to use this application?',
-        },
+// add function to present manager questions
+function addManagerInfo() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'managerName',
+                message: "What is the Manager's name?",
+            },
+            {
+                type: 'input',
+                name: 'managerID',
+                message: "What is the Manager's Employee ID?",
+            },
+            {
+                type: 'input',
+                name: 'managerEmail',
+                message: "What is the Manager's email address?",
+            },
+            {
+                type: 'input',
+                name: 'managerPhone',
+                message: "What is the Manager's phone number?",
+            },
 
-        //below is a checkbox 
-        // {
-        //     type: 'checkbox',
-        //     message: 'What license was used for this application?',
-        //     name: 'license',
-        //     choices: ['MIT', 'IBM', 'ISC'],
-        // },
-        {
-            type: 'input',
-            name: 'contributors',
-            message: 'Did you have any contributors?',
-        },
-        {
-            type: 'input',
-            name: 'tests',
-            message: 'What are the test instructions for this application?',
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'What is your github username for contact?',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What your email for questions?',
-        },
-    ])
-    // console.log(data)
+            // below is a checkbox 
+            {
+                type: 'checkbox',
+                message: 'Which type of team member would you like to add?',
+                name: 'membertype',
+                choices: ["Engineer', 'Intern', 'I don't want to add any more team members"],
+            }
+        ]);
+
+    if (choices === "Engineer") {
+        addEngineer();
+    } else if (choices === "Intern"){
+        addIntern();
+    } else return;
+};
+
+//add function to present engineer questions
+function addEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'engineerName',
+                message: "What is the Engineer's name?",
+            },
+            {
+                type: 'input',
+                name: 'engineerID',
+                message: "What is the Engineer's Employee ID?",
+            },
+            {
+                type: 'input',
+                name: 'engineerEmail',
+                message: "What is the Engineer's email address?",
+            },
+            {
+                type: 'input',
+                name: 'engineerPhone',
+                message: "What is the Engineer's phone number?",
+            },
+
+            // below is a checkbox 
+            {
+                type: 'checkbox',
+                message: 'Which type of team member would you like to add?',
+                name: 'membertype',
+                choices: ["Engineer', 'Intern', 'I don't want to add any more team members"],
+            }
+        ]);
+    if (choices === "Engineer") {
+        addEngineer();
+    } else if (choices === "Intern"){
+        addIntern();
+    } else return;
+};
+
+function addIntern() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'internName',
+                message: "What is the Intern's name?",
+            },
+            {
+                type: 'input',
+                name: 'internID',
+                message: "What is the Intern's Employee ID?",
+            },
+            {
+                type: 'input',
+                name: 'internEmail',
+                message: "What is the Intern's email address?",
+            },
+            {
+                type: 'input',
+                name: 'internPhone',
+                message: "What is the Intern's phone number?",
+            },
+
+            // below is a checkbox 
+            {
+                type: 'checkbox',
+                message: 'Which type of team member would you like to add?',
+                name: 'membertype',
+                choices: ["Engineer', 'Intern', 'I don't want to add any more team members"],
+            }
+        ]);
+        if (choices === "Engineer") {
+            addEngineer();
+        } else if (choices === "Intern"){
+            addIntern();
+        } else return;
+}
+
+
+function renderFile(data) {
     .then((data) => {
-        const filename = `teamProfile.html`;
+    const filename = `teamProfile.html`;
 
-        fs.writeFile(filename, 
-            `# ${data.title}
+    fs.writeFile(filename,
+        `# ${data.title}
 ## Table of Contents
 * [Description](##description)
 * [Installation](##installation)
@@ -89,23 +154,8 @@ ${data.tests}
 Please see my github | Contact me for questions
 ------------ | -------------
 [Github Profile](https://github.com/${data.github}) | ${data.email}`,
-            (err) =>
+        (err) =>
             err ? console.log(err) : console.log('Success!')
-        )
-    })
-
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-    if(license == "MIT"){
-        return ('[![License: MIT](https://img.shields.io/badge/License-MIT-blue)](https://opensource.org/licenses/MIT)')
-    } else if (license == "IBM") {
-        return ('[![License: IBM](https://img.shields.io/badge/License-IBM-blue)](https://opensource.org/licenses/IBM)')
-    } else if (license == "ISC") {
-        return ('[![License: ISC](https://img.shields.io/badge/License-ISC-blue)](https://opensource.org/licenses/ISC)')
-    } else {
-        return (' ')
-    }
-    
-};
+    )
+})
+}
